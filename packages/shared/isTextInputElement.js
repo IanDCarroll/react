d163c8147f53213073a185587729ec2796caa150
Ian Carroll <ian.d.carroll@gmail.com>
2019-06-20 11:02:37 -0700

    Comment out untested behavior

--- packages/shared/isTextInputElement.js
+++ packages/shared/isTextInputElement.js
@@ -11,21 +11,21 @@
  * @see http://www.whatwg.org/specs/web-apps/current-work/multipage/the-input-element.html#input-type-attr-summary
  */
 const supportedInputTypes: {[key: string]: true | void} = {
-  color: true,
-  date: true,
-  datetime: true,
-  'datetime-local': true,
-  email: true,
-  month: true,
+  // color: true,
+  // date: true,
+  // datetime: true,
+  // 'datetime-local': true,
+  // email: true,
+  // month: true,
   number: true,
-  password: true,
+  // password: true,
   range: true,
-  search: true,
-  tel: true,
+  // search: true,
+  // tel: true,
   text: true,
-  time: true,
-  url: true,
-  week: true,
+  // time: true,
+  // url: true,
+  // week: true,
 };
 
 function isTextInputElement(elem: ?HTMLElement): boolean {
